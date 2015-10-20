431cb30bb4b69481af401c0630499e76eb0394e3
Ken Wilcox <wilcoxk@gmail.com>
2015-10-19 21:08:19 -0600

    Loading up non https site

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+  
+  var webView: WKWebView!
+  
+  override func loadView() {
+    webView = WKWebView()
+    webView.navigationDelegate = self
+    view = webView
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let url = NSURL(string: "http://www.starfall.com")!
+    webView.loadRequest(NSURLRequest(URL: url))
+    webView.allowsBackForwardNavigationGestures = true
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
 }
 
+extension ViewController: WKNavigationDelegate {
+  
+}
